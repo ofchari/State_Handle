09ffd72399ad0d424bfcbf630115ab36ca6e4c82
ofchari <ofchari4@gmail.com>
2024-07-17 19:07:03 +0530

    reference

--- lib/view/Screens/home.dart
+++ lib/view/Screens/home.dart
@@ -54,7 +54,7 @@ class _HomeState extends State<Home> {
             ),
             PageView(
               onPageChanged: (index){
-                pagee.currentIndex;
+                pagee.pagess(pagee.currentIndex);
               },
             )
 
